f176be8cadfb4659e7feb093f516439065275462
Alexander Dmitriev <alex1998dmit@gmail.com>
2020-05-24 16:17:27 +0300

    Updates

---
+++ precache-manifest.9c2eb1241ac5c71dc109cff155276e83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d77329b0d85ebe7cf4ed9028780c99",
+    "url": "/maroomtest/index.html"
+  },
+  {
+    "revision": "5fb3fff95d524d2f5cec",
+    "url": "/maroomtest/static/css/main.101402f3.chunk.css"
+  },
+  {
+    "revision": "9ff0ae4c57ab8de229e0",
+    "url": "/maroomtest/static/js/2.3632223c.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/maroomtest/static/js/2.3632223c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb3fff95d524d2f5cec",
+    "url": "/maroomtest/static/js/main.30f61e97.chunk.js"
+  },
+  {
+    "revision": "71c9bff4f98c4eaa8686",
+    "url": "/maroomtest/static/js/runtime-main.41ff19b4.js"
+  },
+  {
+    "revision": "0d243c4a3bbee7ac52e2e0628395bb89",
+    "url": "/maroomtest/static/media/question.0d243c4a.svg"
+  }
+]);
